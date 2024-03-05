96a057d283598be27cb9816a57ba6fe7c0c712a4
GitRemka <roman141999@mail.ru>
2024-03-05 13:11:54 +0700

    Добавляет модуль, отвечающий за отрисовку миниатюр

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import {generatedPosts} from './data.mjs';
+import {renderThumbnails} from './thumbnails.mjs';
+
+const pictures = generatedPosts();
+renderThumbnails(pictures);
 
 // eslint-disable-next-line no-console
-console.table(generatedPosts());
+console.table(pictures);
