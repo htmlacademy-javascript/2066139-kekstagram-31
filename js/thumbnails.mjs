f7bfcb8b32d23180c483be6ba0b4fc45315c6969
GitRemka <roman141999@mail.ru>
2024-03-14 15:18:45 +0700

    Изменяет бизнес-логику и отказывается от делегирования

--- js/thumbnails.mjs
+++ js/thumbnails.mjs
@@ -1,11 +1,9 @@
-import {openUserPostModal} from './full-photo-modal.mjs';
-
 const thumbnailContainer = document.querySelector('.pictures');
 const thumbnailTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const createThumbnail = ({urlPhoto, description, likes, comments}, index) => {
+const createThumbnail = ({urlPhoto, description, likes, comments}, imageClickHandler) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
   const thumbnailImgElement = thumbnail.querySelector('.picture__img');
 
@@ -13,34 +11,20 @@ const createThumbnail = ({urlPhoto, description, likes, comments}, index) => {
   thumbnailImgElement.alt = description;
   thumbnail.querySelector('.picture__likes').textContent = likes;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
-  thumbnail.setAttribute('data-index', index);
+  thumbnailImgElement.addEventListener('click', () => imageClickHandler({urlPhoto, description, likes, comments}));
 
   return thumbnail;
 };
 
-const onThumbnailClick = (evt, pictures) => {
-  const thumbnailLink = evt.target.closest('a.picture');
-
-  if (thumbnailLink) {
-
-    const thumbnailItem = pictures[thumbnailLink.dataset.index];
-
-    if (thumbnailItem) {
-      openUserPostModal(thumbnailItem);
-    }
-  }
-};
-
-const renderThumbnails = (pictures) => {
+const renderThumbnails = (pictures, onThumbnailClick) => {
   const thumbnailListFragment = document.createDocumentFragment();
 
-  pictures.forEach((picture, index) => {
-    const thumbnailElement = createThumbnail(picture, index);
+  pictures.forEach((picture) => {
+    const thumbnailElement = createThumbnail(picture, onThumbnailClick);
     thumbnailListFragment.append(thumbnailElement);
   });
 
   thumbnailContainer.append(thumbnailListFragment);
-  thumbnailContainer.addEventListener('click', (evt) => onThumbnailClick(evt, pictures));
 };
 
-export {renderThumbnails, onThumbnailClick};
+export {renderThumbnails};
